5bd74793041319d2e31be5c1cdcce895318bce01
Ziphozenkosimthombe <ziphomthombe478@gmail.com>
2024-04-17 18:28:20 +0200

    introduction

---
+++ C#_Learning/Introduction/displayingOutput.cs
@@ -0,0 +1,14 @@
+/*
+In this exercise, you will try printing multiple objects using the Console.WriteLine statement
+*/ 
+
+using System;
+
+class Display
+{
+    public static void print()
+    {
+      //write your code here
+      Console.WriteLine("The movie quote is: My name is Bond. James Bond!");
+    }
+}
